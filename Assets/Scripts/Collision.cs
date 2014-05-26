e8031c03e0d2f8ab5e18b8ba68a0b1fee17178e5
rob verhoef <16617@ma-web.nl>
2014-05-26 07:03:19 +0200

    Added Audio and other new files

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Collision : MonoBehaviour {
 void OnTriggerEnter2D(Collider2D col){
-		if (col.gameObject.tag == "Cube")
+		//when the player collides with an obstacle, he dies.
+		if (col.gameObject.tag == "Obstacle")
 			Debug.Log("Dead");
 		} 
 }
--- Assets/Scripts/Movement_Control.cs
+++ Assets/Scripts/Movement_Control.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class Movement_Control : MonoBehaviour {
 
 	void Update () {
+		//When the player presses up, he jumps
 		if (Input.GetKeyDown ("up"))
 		animation.Play ("Jump", PlayMode.StopAll);
-
+		//When the player presses down, he slides
 		if (Input.GetKeyDown ("down"))
 			animation.Play ("Crouch", PlayMode.StopAll);
 	}
