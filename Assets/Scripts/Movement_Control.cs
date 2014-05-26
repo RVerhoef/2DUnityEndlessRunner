264bd9d5df4e39e73385fa644485da91ceba62a4
rob verhoef <16617@ma-web.nl>
2014-05-26 11:11:39 +0200

    Updated player

--- Assets/Scripts/Movement_Control.cs
+++ Assets/Scripts/Movement_Control.cs
@@ -6,7 +6,7 @@ public class Movement_Control : MonoBehaviour {
 	void Update () {
 		//When the player presses up, he jumps
 		if (Input.GetKeyDown ("up"))
-		animation.Play ("Jump", PlayMode.StopAll);
+		animation.Play ("Run", PlayMode.StopAll);
 		//When the player presses down, he slides
 		if (Input.GetKeyDown ("down"))
 			animation.Play ("Crouch", PlayMode.StopAll);
