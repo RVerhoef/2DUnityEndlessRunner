c6fa194032dae5ce527c0211dd01bfda81424c46
rob verhoef <16617@ma-web.nl>
2014-06-02 16:36:01 +0200

    Reversing the fix.

---
+++ Assets/Scripts/Star_Thrower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star_Thrower : MonoBehaviour {
+
+	public Transform Star;
+
+	//Shoots a star when the left mouse button is pressed
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Mouse2)) 
+		{
+			ShootStar();		
+		}
+		
+	}
+
+	//Puts the star into the game world
+	void ShootStar()
+	{
+		Instantiate(Star, this.transform.position, this.transform.rotation);
+
+	}
+}
